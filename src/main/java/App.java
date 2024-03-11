96c798d36835dd895fea81496f57e95ae10b5c60
whoisthatme <Ychinin@yandex.ru>
2024-03-11 18:14:09 +0300

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,27 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+
+
+        ApplicationContext applicationContextCat = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println(beanCat.getName());
+        System.out.println(beanCat2.getName());
+
+        System.out.println(bean == bean2);
+        System.out.println(beanCat == beanCat2);
+
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,11 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCatName() {
+        Cat cat = new Cat();
+        cat.setName("Name");
+        return cat;
+    }
 }
